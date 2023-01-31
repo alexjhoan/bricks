5acce741655a370826a03fa717d79ac1ce9c114c
anaismiraira <mirairaanais@gmail.com>
2023-01-31 14:52:38 -0400

    1452 31012023 contacto

--- assets/index.js
+++ assets/index.js
@@ -43,3 +43,60 @@ if (screen.width < 768) {
 }
 /*------------------------------Bussines----------------------------------*/
 
+// ------------------------------Form-----------------------------
+
+function dataSubmited(data) {
+  const requestOptions = {
+    method: 'POST',
+    body: data,
+    headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+    },
+  };
+  fetch("https://www.infocasas.com.uy?mid=formulario&func=ajax_save&json=1", requestOptions)
+  .then((json) => {
+    setTimeout(()=>{
+      if (json.status === 200) {
+        $('#formSuccess').fadeIn();
+      } else {
+        $('#formError').fadeIn();
+      }
+      $('#formSending').hide();
+    }, 2000)
+  })
+  .catch(error => {
+    console.log('error', error);
+    setTimeout(() => {
+      $('#formSending').hide();
+      $('#formError').fadeIn();
+    }, 100)
+  });
+}
+
+function submited() {
+  const form = document.querySelector('#contactForm')
+  const data = JSON.stringify({
+    nombre: form.name.value,
+    apellido: "",
+    email: form.email.value,
+    telefono: form.phone.value,
+    tel: form.phone.value,
+    source: 2,
+    utm_source: "web_cliente",
+    utm_medium: "continents",
+    extra: form.consult.value,
+    IDpais: 1,
+    IDform: 447
+  })
+  event.preventDefault()
+  if (!form.checkValidity()) {
+    event.stopPropagation()
+  }else{
+    dataSubmited(data)
+    setTimeout(()=>{
+      $(form).fadeOut();
+      $('#formSending').fadeIn();
+    },300)
+  }
+  form.classList.add('was-validated')
+}
