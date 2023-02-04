c2e7e007fc46f95eea9830f77f19d45ecc426b90
alexjhoan <alexjhoan.25@gmail.com>
2023-02-04 15:40:35 -0400

    add animation to navbar

--- assets/index.js
+++ assets/index.js
@@ -1,19 +1,43 @@
-$(window).on('load', function () {
-  $('body').css('opacity', '1');
+$(window).on("load", function () {
+  $("body").css("opacity", "1");
+  $(".linkTo").click(function (e) {
+    e.preventDefault();
+    $("header .collapse.show").removeClass("show");
+    const url = $(this).attr("href");
+    const header = $("header").height();
+    if (!url.includes("html")) {
+      const section = $(url.slice(1)).offset().top;
+      window.scrollTo({ top: section - header, behavior: "smooth" });
+    } else {
+      window.location = url;
+    }
+  });
 });
 
-let offset
+let offset;
 
-if (screen.width > 768){
-  offset = 200
+if (screen.width > 768) {
+  offset = 200;
 } else {
-  offset = 0
+  offset = 0;
 }
 
-new WOW({offset:offset, scrollContainer: null}).init()
+new WOW({ offset: offset, scrollContainer: null }).init();
 
-$('header').load('components/header.html')
-$('footer').load('components/footer.html')
+$("header").load("components/header.html");
+$("footer").load("components/footer.html");
+
+/*---------------------------------Header------------------------------------*/
+
+$(window).on("scroll", function () {
+  const scroll = $(window).scrollTop();
+  const header = $("header");
+  if (scroll > header.height()) {
+    header.addClass("nav-down");
+  } else {
+    header.removeClass("nav-down");
+  }
+});
 
 /*-------------------------------Market Grid----------------------------------*/
 if (screen.width < 992) {
@@ -49,44 +73,47 @@ if (screen.width < 992) {
 /*------------------------------Bussines----------------------------------*/
 
 function seeMore() {
-  let visibleItems = $('#customers .item:visible').length + ImgMore
-  $('#customers .item:lt('+visibleItems+')').fadeIn(800);
-  if(visibleItems >= items) {
-    $('.btnMore').hide();
+  let visibleItems = $("#customers .item:visible").length + ImgMore;
+  $("#customers .item:lt(" + visibleItems + ")").fadeIn(800);
+  if (visibleItems >= items) {
+    $(".btnMore").hide();
   }
 }
 /*------------------------------Form-----------------------------*/
 
 function dataSubmited(data) {
   const requestOptions = {
-    method: 'POST',
+    method: "POST",
     body: data,
     headers: {
-    'Content-type': 'application/json; charset=UTF-8',
+      "Content-type": "application/json; charset=UTF-8",
     },
   };
-  fetch("https://www.infocasas.com.uy?mid=formulario&func=ajax_save&json=1", requestOptions)
-  .then((json) => {
-    setTimeout(()=>{
-      if (json.status === 200) {
-        $('#formSuccess').fadeIn();
-      } else {
-        $('#formError').fadeIn();
-      }
-      $('#formSending').hide();
-    }, 2000)
-  })
-  .catch(error => {
-    console.log('error', error);
-    setTimeout(() => {
-      $('#formSending').hide();
-      $('#formError').fadeIn();
-    }, 100)
-  });
+  fetch(
+    "https://www.infocasas.com.uy?mid=formulario&func=ajax_save&json=1",
+    requestOptions
+  )
+    .then((json) => {
+      setTimeout(() => {
+        if (json.status === 200) {
+          $("#formSuccess").fadeIn();
+        } else {
+          $("#formError").fadeIn();
+        }
+        $("#formSending").hide();
+      }, 2000);
+    })
+    .catch((error) => {
+      console.log("error", error);
+      setTimeout(() => {
+        $("#formSending").hide();
+        $("#formError").fadeIn();
+      }, 100);
+    });
 }
 
 function submited() {
-  const form = document.querySelector('#contactForm')
+  const form = document.querySelector("#contactForm");
   const data = JSON.stringify({
     nombre: form.name.value,
     apellido: "",
@@ -98,32 +125,32 @@ function submited() {
     utm_medium: "continents",
     extra: form.consult.value,
     IDpais: 1,
-    IDform: 447
-  })
-  event.preventDefault()
+    IDform: 447,
+  });
+  event.preventDefault();
   if (!form.checkValidity()) {
-    event.stopPropagation()
-  }else{
-    dataSubmited(data)
-    setTimeout(()=>{
+    event.stopPropagation();
+  } else {
+    dataSubmited(data);
+    setTimeout(() => {
       $(form).fadeOut();
-      $('#formSending').fadeIn();
-    },300)
+      $("#formSending").fadeIn();
+    }, 300);
   }
-  form.classList.add('was-validated')
+  form.classList.add("was-validated");
 }
 
 let maxHeight = function (elems) {
   return Math.max.apply(
-      null,
-      elems
-          .map(function () {
-              return $(this).height();
-          })
-          .get()
+    null,
+    elems
+      .map(function () {
+        return $(this).height();
+      })
+      .get()
   );
 };
 if (screen.width > 992) {
-let maxHeightTitle = maxHeight($("#marketing .item .item_content"));
-$("#marketing .item .item_content").css("height", +maxHeightTitle + "px");
+  let maxHeightTitle = maxHeight($("#marketing .item .item_content"));
+  $("#marketing .item .item_content").css("height", +maxHeightTitle + "px");
 }
